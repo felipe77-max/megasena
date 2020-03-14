1a412945c28de81ebb7ae0af72481fbf7824a84b
Felipe Jose <f_js07@hotmail.com>
2020-03-13 22:26:44 -0300

    Criação de numeros da megasena em php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Megasena</title>
+<body>
+
+
+    <div class="wrapper">
+        <h1>Megasena</h1>
+            <div class="numeros" id="n1">
+            <h3>n1</h3>
+              </div>
+                <div class="numeros" id="n2">
+                  <h3>n2</h3>    
+                 </div>        
+                <div class="numeros" id="n3">
+                    <h3>n3</h3>
+                </div>
+                    <div class="numeros" id="n4">
+                        <h3>n4</h3>
+                    </div>
+                        <div class="numeros" id="n5">
+                            <h3>n5</h3>
+                        </div>
+                                <div class="numeros" id="n6">
+                                    <h3>n6</h3>
+                                </div>
+        <div class="encerramento">
+            <button>Estou com Sorte</button> 
+        </div>
+    </div>
+
+
+</body>
+</html>
+<?php
+
+
+?>
